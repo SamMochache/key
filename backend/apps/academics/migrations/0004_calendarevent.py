def5c3b1b13c9c614dde7a9321a6f065ff248f98
Sam Mochache <sammochache01@gmail.com>
2026-09-08 01:09:43 +0300

    fix(calendar): use standard UUID migration

--- backend/apps/academics/migrations/0004_calendarevent.py
+++ backend/apps/academics/migrations/0004_calendarevent.py
@@ -1,5 +1,6 @@
 from django.db import migrations, models
 import django.db.models.deletion
+import uuid
 
 
 class Migration(migrations.Migration):
@@ -11,7 +12,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name="CalendarEvent",
             fields=[
-                ("id", models.UUIDField(editable=False, primary_key=True, serialize=False)),
+                ("id", models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
                 ("created_at", models.DateTimeField(auto_now_add=True)),
                 ("updated_at", models.DateTimeField(auto_now=True)),
                 ("deleted_at", models.DateTimeField(blank=True, null=True)),
