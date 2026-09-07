8011dd8c03f473cb1017ef4d290f0c4f94e31355
Sam Mochache <sammochache01@gmail.com>
2026-09-07 13:46:45 +0300

    feat: register student API routes

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("api/", include("core.urls")),
     path("api/", include("apps.identity.api_urls")),
     path("api/", include("apps.schools.api_urls")),
+    path("api/", include("apps.students.api_urls")),
     path("api/", include("apps.assessments.api_urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
