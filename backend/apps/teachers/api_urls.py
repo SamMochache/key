4f8eb04b29eb3153ba540c5fef7357b4be8ef0bf
Sam Mochache <sammochache01@gmail.com>
2026-09-07 15:20:29 +0300

    feat(teachers): register department API routes

--- backend/apps/teachers/api_urls.py
+++ backend/apps/teachers/api_urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import TeacherViewSet
+from .views import DepartmentViewSet, TeacherViewSet
 
 router = DefaultRouter()
 router.register("teachers", TeacherViewSet, basename="teacher")
+router.register("departments", DepartmentViewSet, basename="department")
 
 urlpatterns = router.urls
