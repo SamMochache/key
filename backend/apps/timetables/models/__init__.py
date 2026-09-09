871fd5ed2601a62dacd186ce0fabd117b137662d
Sam Mochache <sammochache01@gmail.com>
2026-09-09 13:00:07 +0300

    fix(timetables): export all timetable models

--- backend/apps/timetables/models/__init__.py
+++ backend/apps/timetables/models/__init__.py
@@ -1,5 +1,9 @@
+from .period import Period
+from .timetable import Timetable
 from .timetable_entry import TimetableEntry
 
-__all__=[
+__all__ = [
+    "Period",
+    "Timetable",
     "TimetableEntry",
 ]
